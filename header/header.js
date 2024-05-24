a362491c67cf0d1295083bcee3ddc55650f930f9
Leibril <ariabi.sr@gmail.com>
2024-05-24 17:50:05 -0600

    header hecho por Ariel

---
+++ header/header.js
@@ -0,0 +1,17 @@
+let headerXAriel = document.querySelector(".header");
+
+headerXAriel.innerHTML = `
+
+    <div class="pUser">
+        <h1>💖 Hellen 💖</h1>
+    </div>
+
+    <div class="pBot">
+        <a href="">👤 Mi Cuenta</a>
+        <a href="">❤️ Mis favoritos</a>
+        <a href="">Github</a>
+    </div>
+
+
+
+`;
